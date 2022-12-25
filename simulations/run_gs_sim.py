f5825e6f2866b6900559098c73c1221955882051
Soumajyoti <sarkar.soumajyoti@gmail.com>
2022-12-25 06:26:43 -0800

    Updates stacked 12/25

--- simulations/run_gs_sim.py
+++ simulations/run_gs_sim.py
@@ -4,10 +4,10 @@ import multiprocessing
 import os
 import dill
 
-VARIANCE = 1.0
+VARIANCE = 0.2
 NUM_SIMS_PER_STEP = 1
-T = 2000 # horizon, n
-SAVE_DIR = "../figures/figure_01_09_0p5_var1/20b_60l"
+T = 50 # horizon, n
+SAVE_DIR = "../figures/figure_02_06_0p5_var1/100b_20l"
 
 os.makedirs(SAVE_DIR, exist_ok=True)
 os.makedirs(SAVE_DIR + "/phases/", exist_ok=True)
@@ -27,17 +27,17 @@ if __name__ == "__main__":
     borrower_matches_optimal = -1
     print("-----------------------------------Optimal matching-----------------------------------")
     while borrower_matches_optimal == -1:
-        u_b, u_l, c, q, preference_borrowers, preference_lenders = simulate_lending()
+        alpha_t, u_b, u_l, c, q, preference_borrowers, preference_lenders = simulate_lending()
         rewards_from_borrowers = get_rewards_list_start(len(u_l), len(u_b), u_b, NUM_SIMS_PER_STEP, T, VARIANCE)
 
         lambda_1 = 0.5
         lambda_2 = 0.5
-        borrower_matches_optimal, lender_matches_optimal, objVal = gs_bandit_method_baseline(u_b, u_l, c, q, lambda_1,
+        borrower_matches_optimal, lender_matches_optimal, objVal = gs_bandit_method_baseline(alpha_t, u_b, u_l, c, q, lambda_1,
                                                                                              lambda_2)  # baseline
 
     print("-----------------------------------GS Basic-----------------------------------")
     regret_lenders_basic, arms_ts, arms_ts_mostpref, borrower_matches, lender_matches, objVal = gs_bandit_method_basic(
-                                                                                                u_b, u_l, c, q, lambda_1,
+                                                                                                alpha_t, u_b, u_l, c, q, lambda_1,
                                                                                                 lambda_2,  preference_borrowers,
                                                                                                 preference_lenders, NUM_SIMS_PER_STEP, T,
                                                                                                 rewards_from_borrowers, VARIANCE,
